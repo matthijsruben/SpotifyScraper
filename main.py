08ac2925484f38abbb3f620e76836a3bc1b69054
s1997807 <m.r.kok@student.utwente.nl>
2020-12-15 10:38:09 +0100

    secured API SECRET

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import os
 import datetime
 import time
 
@@ -14,7 +15,7 @@ def get_access_token():
     # both are connected to the dashboard application 'SpotifyScraper' connected to our spotify account
     # Accessible via 'https://developer.spotify.com/dashboard'
     CLIENT_ID = "35f1893419734fac9245f8b04101960c"
-    CLIENT_SECRET = "25663d57829545e38346d8e34bf84e71"
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET_SPOTIFY_API")
     base64_encoded = base64.b64encode((CLIENT_ID + ":" + CLIENT_SECRET).encode("ascii")).decode("ascii")
 
     # Officially, all parameters of the post request must be encoded in application/x-www-form-urlencoded
@@ -24,9 +25,11 @@ def get_access_token():
 
     ACCESS_TOKEN = auth_response.json()["access_token"]
     expires_in = auth_response.json()["expires_in"]
-    token_type = auth_response.json()["token_type"] # should be just a string "Bearer"
+    token_type = auth_response.json()["token_type"]  # should be just a string "Bearer"
     return ACCESS_TOKEN, expires_in, token_type
 
+
+def 
 
 # I guess we need threads to check every ~hour to refresh tokens
 # BUFFER = 60  # 1 minute buffer just to be sure
@@ -34,9 +37,9 @@ def get_access_token():
 # if datetime.datetime.now() > start+datetime.timedelta(seconds=expires_in-BUFFER):
 #     ACCESS_TOKEN, expires_in, token_type = get_access_token()
 
+# ACCESS_TOKEN expires in exactly 1 hour. Then a new ACCESS_TOKEN needs to be requested.
 ACCESS_TOKEN, expires_in, token_type = get_access_token()
 
-# ACCESS_TOKEN expires in exactly 1 hour. Then a new ACCESS_TOKEN needs to be requested.
 response = requests.get("https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V",
                         headers={"Authorization": token_type + " " + ACCESS_TOKEN})
 print(response.json())
